bfa1ae070639faa7d7bf6a99dbcfad8a3321ed2c
Indra Saputra <flame.fire99@gmail.com>
2018-05-16 11:46:11 +0700

    create constant backoff struct

--- backoff.go
+++ backoff.go
@@ -15,3 +15,12 @@ type Intervaler interface {
 	// Interval defines the n-th interval.
 	Interval(order int) time.Duration
 }
+
+// ConstantBackoff implements Intervaler using constant interval.
+type ConstantBackoff struct {
+	// BackoffInterval defines how long the next interval will be, compared to the previous one.
+	BackoffInterval time.Duration
+	// JitterInterval defines the additional value for interval.
+	// The additional value will be in range [0,JitterInterval].
+	JitterInterval time.Duration
+}

41b85c5c03c35d65968ba57a5fc6ed26c40c39e7
Indra Saputra <flame.fire99@gmail.com>
2018-05-16 16:13:04 +0700

    Exponential Backoff (#4)
    * create exp backoff struct
    
    * add max interval and multiplier fields
    
    * change var name from state to counter
    
    * implement nextinterval for exp backoff
    
    * implement reset
    
    * test if exp backoff implement backoff
    
    * *ups, test for reset method in constant backoff
    
    * max interval is used if set
    
    * test for exp backoff reset
    
    * multiplier should be at least 1
    
    * test for next interval exponential backoff

--- backoff.go
+++ backoff.go
@@ -1,6 +1,7 @@
 package backoff
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -40,4 +41,55 @@ func (c *ConstantBackoff) NextInterval() time.Duration {
 // Actually, it does nothing
 // since constant backoff will always constant all the time.
 func (c *ConstantBackoff) Reset() {
+}
+
+// ExponentialBackoff implements Backoff using exponential interval.
+type ExponentialBackoff struct {
+	// BackoffInterval defines how long the next interval will be, compared to the previous one.
+	BackoffInterval time.Duration
+	// JitterInterval defines the additional value for interval.
+	// The additional value will be in range [0,JitterInterval).
+	JitterInterval time.Duration
+	// MaxInterval defines the maximum interval allowed.
+	// If this field is let empty,
+	// it means that there is no maximum value for interval.
+	// Please, keep in mind that if this field is empty,
+	// the interval can be a very long time.
+	MaxInterval time.Duration
+	// Multipler defines the multipler for the next interval.
+	// Default value for Multiplier is 1.
+	// Using `Multiplier = 1` means ExponentialBackoff can behave
+	// like ConstantBackoff.
+	Multiplier int
+	// counter defines how many times NextInterval is called.
+	counter int
+}
+
+// NextInterval returns next interval.
+func (e *ExponentialBackoff) NextInterval() time.Duration {
+	// minimum multiplier should be 1
+	multipler := math.Max(1, float64(e.Multiplier))
+
+	exponent := math.Pow(multipler, float64(e.counter))
+	backoffInterval := float64(e.BackoffInterval) * exponent
+
+	// prevent interval to exceed max interval
+	if e.MaxInterval > 0 {
+		backoffInterval = math.Min(backoffInterval, float64(e.MaxInterval))
+	}
+
+	var jitter int64
+	if e.JitterInterval <= 0 {
+		jitter = 0
+	} else {
+		jitter = rand.Int63n(int64(e.JitterInterval))
+	}
+
+	e.counter++
+	return time.Duration(backoffInterval + float64(jitter))
+}
+
+// Reset resets Exponential Backoff.
+func (e *ExponentialBackoff) Reset() {
+	e.counter = 0
 }
--- backoff_test.go
+++ backoff_test.go
@@ -1,6 +1,7 @@
 package backoff_test
 
 import (
+	"math"
 	"strconv"
 	"testing"
 	"time"
@@ -9,12 +10,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// tests for constant backoff
+
 func TestConstantBackoff_(t *testing.T) {
-	b := &backoff.ConstantBackoff{}
-	assert.Implements(t, (*backoff.Backoff)(nil), b)
+	c := &backoff.ConstantBackoff{}
+	assert.Implements(t, (*backoff.Backoff)(nil), c)
 }
 
-func TestConstantBackoff_Interval(t *testing.T) {
+func TestConstantBackoff_NextInterval(t *testing.T) {
 	backoffInterval := 500 * time.Millisecond
 	jitterInterval := 100 * time.Millisecond
 	c1 := backoff.ConstantBackoff{
@@ -69,4 +72,98 @@ func TestConstantBackoff_Interval(t *testing.T) {
 			assert.Equal(t, backoffInterval, b)
 		})
 	}
+}
+
+func TestConstantBackoff_Reset(t *testing.T) {
+	c := backoff.ConstantBackoff{}
+	assert.NotPanics(t, func() { c.Reset() })
+}
+
+// tests for exponential backoff
+
+func TestExponentialBackoff_(t *testing.T) {
+	b := &backoff.ExponentialBackoff{}
+	assert.Implements(t, (*backoff.Backoff)(nil), b)
+}
+
+func TestExponentialBackoff_Reset(t *testing.T) {
+	backoffInterval := 100 * time.Millisecond
+	e := backoff.ExponentialBackoff{
+		BackoffInterval: backoffInterval,
+	}
+
+	t.Run("exponential backoff reset", func(t *testing.T) {
+		for i := 0; i < 10; i++ {
+			e.NextInterval()
+		}
+
+		e.Reset()
+		b := e.NextInterval()
+		assert.Equal(t, backoffInterval, b)
+	})
+}
+
+func TestExponentialBackoff_NextInterval(t *testing.T) {
+	backoffInterval := 100 * time.Millisecond
+	jitterInterval := 50 * time.Millisecond
+	multiplier := 2
+	maxInterval := 10 * time.Second
+
+	t.Run("exponential backoff only backoff interval", func(t *testing.T) {
+		e := backoff.ExponentialBackoff{
+			BackoffInterval: backoffInterval,
+		}
+
+		for i := 0; i < 100; i++ {
+			b := e.NextInterval()
+			assert.Equal(t, backoffInterval, b)
+		}
+	})
+
+	t.Run("use backoff interval and jitter", func(t *testing.T) {
+		e := backoff.ExponentialBackoff{
+			BackoffInterval: backoffInterval,
+			JitterInterval:  jitterInterval,
+		}
+
+		for i := 0; i < 100; i++ {
+			b := e.NextInterval()
+			assert.True(t, b >= backoffInterval)
+			assert.True(t, b < backoffInterval+jitterInterval)
+		}
+	})
+
+	t.Run("use backoff, jitter, and multiplier", func(t *testing.T) {
+		e := backoff.ExponentialBackoff{
+			BackoffInterval: backoffInterval,
+			JitterInterval:  jitterInterval,
+			Multiplier:      multiplier,
+		}
+
+		// iterate 30 times should be enough
+		// more than 30 can be overflow
+		for i := 0; i < 30; i++ {
+			m := math.Pow(float64(multiplier), float64(i))
+
+			b := e.NextInterval()
+			assert.True(t, b >= backoffInterval*time.Duration(m))
+			assert.True(t, b < backoffInterval*time.Duration(m)+jitterInterval)
+		}
+	})
+
+	t.Run("use backoff, jitter, multiplier, and max interval", func(t *testing.T) {
+		e := backoff.ExponentialBackoff{
+			BackoffInterval: backoffInterval,
+			JitterInterval:  jitterInterval,
+			Multiplier:      multiplier,
+			MaxInterval:     maxInterval,
+		}
+
+		// iterate 30 times should be enough
+		// more than 30 can be overflow
+		for i := 0; i < 30; i++ {
+			b := e.NextInterval()
+			assert.True(t, b <= maxInterval+jitterInterval)
+		}
+	})
 }

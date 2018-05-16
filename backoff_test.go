d73c72b988b7f395659d60e90c8e122312879e39
Indra Saputra <flame.fire99@gmail.com>
2018-05-16 13:00:46 +0700

    test for constant backoff

--- backoff_test.go
+++ backoff_test.go
@@ -1,1 +1,34 @@
 package backoff_test
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/indrasaputra/backoff"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConstantBackoff_Interval(t *testing.T) {
+	backoffInterval := 500 * time.Millisecond
+	jitterInterval := 200 * time.Millisecond
+	c := backoff.ConstantBackoff{
+		BackoffInterval: backoffInterval,
+		JitterInterval:  jitterInterval,
+	}
+
+	for i := -100; i < 1; i++ {
+		t.Run("test:"+strconv.Itoa(i), func(t *testing.T) {
+			b := c.Interval(i)
+			assert.Equal(t, time.Duration(0), b)
+		})
+	}
+
+	for i := 1; i < 100; i++ {
+		t.Run("test:"+strconv.Itoa(i), func(t *testing.T) {
+			b := c.Interval(i)
+			assert.True(t, b >= backoffInterval)
+			assert.True(t, b < backoffInterval+jitterInterval)
+		})
+	}
+}

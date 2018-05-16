f24e1e48b2658bcb4074e4e51a03bbc24685036f
Indra Saputra <flame.fire99@gmail.com>
2018-05-16 11:55:10 +0700

    implement Interval for ConstantBackoff

--- backoff.go
+++ backoff.go
@@ -24,6 +24,16 @@ type ConstantBackoff struct {
 	// BackoffInterval defines how long the next interval will be, compared to the previous one.
 	BackoffInterval time.Duration
 	// JitterInterval defines the additional value for interval.
-	// The additional value will be in range [0,JitterInterval].
+	// The additional value will be in range [0,JitterInterval).
 	JitterInterval time.Duration
+}
+
+// Interval returns n-th interval.
+func (c *ConstantBackoff) Interval(order int) time.Duration {
+	if order <= 0 {
+		return 0
+	}
+
+	jitter := rand.Int63n(int64(c.JitterInterval))
+	return time.Duration(c.BackoffInterval + time.Duration(jitter))
 }
